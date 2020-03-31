251fa00d78592d728e80349e0d4e97cab01b1786
Nick Maludy <nmaludy@gmail.com>
2020-03-31 09:38:41 -0400

    Initial work on HTTP module, so i don't lose it again

---
+++ tasks/download.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require_relative '../../ruby_task_helper/files/task_helper.rb'
+
+# Bolt task for sending an HTTP GET request
+class HttpDownloadTask < TaskHelper
+  def add_module_lib_paths(install_dir)
+    Dir.glob(File.join([install_dir, '*'])).each do |mod|
+      $LOAD_PATH << File.join([mod, 'lib'])
+    end
+  end
+
+  def task(url: nil,
+           body: nil,
+           headers: {},
+           **kwargs)
+    add_module_lib_paths(kwargs[:_installdir])
+    require 'puppet_x/encore/http/client'
+
+    ssl_verify = if kwargs.fetch(:ssl_verify, true)
+                   OpenSSL::SSL::VERIFY_PEER
+                 else
+                   OpenSSL::SSL::VERIFY_NONE
+                 end
+    http = PuppetX::Http::Client.new(username: kwargs[:username],
+                                     password: kwargs[:password],
+                                     ssl: kwargs.fetch(:ssl, true),
+                                     ssl_verify: ssl_verify,
+                                     redirect_limit: kwargs.fetch(:redirect_limit, 10))
+
+    response = http.get(url, body: body, headers: headers)
+    open("sample.flv", "wb") do |file|
+      file.write(resp.body)
+    end
+  end
+end
+
+HttpDownloadTask.run if $PROGRAM_NAME == __FILE__
